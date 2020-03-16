63cd52ecccf35c07fb3bcb91c0cc249e71604e0f
adel <adelbensalem1992@gmail.com>
2020-03-16 23:55:39 +0100

    Updates

---
+++ precache-manifest.9b79c6aa3e6d5146462042fb9be524ba.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fb14b8b5c8813ffc969cf242d3c58b",
+    "url": "/alkhatt/index.html"
+  },
+  {
+    "revision": "c503371c9c27cec69b15",
+    "url": "/alkhatt/static/css/main.f4542167.chunk.css"
+  },
+  {
+    "revision": "0599bb576557d8eb18b6",
+    "url": "/alkhatt/static/js/2.4195672d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/alkhatt/static/js/2.4195672d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c503371c9c27cec69b15",
+    "url": "/alkhatt/static/js/main.9d666fac.chunk.js"
+  },
+  {
+    "revision": "8d517d5950a13374a52a",
+    "url": "/alkhatt/static/js/runtime-main.378c305b.js"
+  },
+  {
+    "revision": "5037f40205b10a45732c0aaa32782373",
+    "url": "/alkhatt/static/media/Background.5037f402.jpg"
+  },
+  {
+    "revision": "be27b8c646a18839353277efe1210ec3",
+    "url": "/alkhatt/static/media/FuturaLT-Light.be27b8c6.ttf"
+  },
+  {
+    "revision": "50164d48e903070809a84f6737493ab5",
+    "url": "/alkhatt/static/media/FuturaLT-Oblique.50164d48.ttf"
+  },
+  {
+    "revision": "cfe815507ca60001a0fabb4407018643",
+    "url": "/alkhatt/static/media/IMG-1.cfe81550.jpg"
+  },
+  {
+    "revision": "6f7298d8b639760c5f9f6088b13f44fb",
+    "url": "/alkhatt/static/media/IMG-2.6f7298d8.jpg"
+  },
+  {
+    "revision": "f30d1843a70e15a8c18cc716f7d91eee",
+    "url": "/alkhatt/static/media/IMG-3.f30d1843.jpg"
+  },
+  {
+    "revision": "a3203686f57ad8cd49f707b87b534c04",
+    "url": "/alkhatt/static/media/Logo-MOM.a3203686.png"
+  },
+  {
+    "revision": "357e9a5a35f9f9dfbc0e07e48a346671",
+    "url": "/alkhatt/static/media/alkhatt-logo.357e9a5a.png"
+  },
+  {
+    "revision": "fa6976e3e44f3c27f4d7e48138b82631",
+    "url": "/alkhatt/static/media/capjc-logo.fa6976e3.png"
+  },
+  {
+    "revision": "b1e5c5d161c1ba4e7479ad9d6c1d21b8",
+    "url": "/alkhatt/static/media/dcaf-logo.b1e5c5d1.png"
+  },
+  {
+    "revision": "7871b2dcc6c64a9723882884700566a7",
+    "url": "/alkhatt/static/media/footer-background.7871b2dc.jpg"
+  },
+  {
+    "revision": "eff33a3dc6aa1881fc606c0b13641f8b",
+    "url": "/alkhatt/static/media/inkube-logo.eff33a3d.png"
+  },
+  {
+    "revision": "9bfdcc23095614b407038f7ebeecc617",
+    "url": "/alkhatt/static/media/inkyfada-logo.9bfdcc23.png"
+  },
+  {
+    "revision": "8d53750ff5f55ff6f463f1daf1f2104b",
+    "url": "/alkhatt/static/media/innovation-icon.8d53750f.png"
+  },
+  {
+    "revision": "9549cf3ffba08da2e4f96525425a8b3b",
+    "url": "/alkhatt/static/media/jaeidaty-logo.9549cf3f.png"
+  },
+  {
+    "revision": "032a02baf41ecea9f71bafaacbebe3f0",
+    "url": "/alkhatt/static/media/osf-logo.032a02ba.png"
+  },
+  {
+    "revision": "aafd36b8f1d180b260bbe6edceb415b6",
+    "url": "/alkhatt/static/media/plaidoyer-icon.aafd36b8.png"
+  },
+  {
+    "revision": "42dd6ea1a2d7a943963deaa4772eee7e",
+    "url": "/alkhatt/static/media/rsf-logo.42dd6ea1.png"
+  },
+  {
+    "revision": "2c087252f66149ef4637fd155ceb90ed",
+    "url": "/alkhatt/static/media/snjt-logo.2c087252.png"
+  }
+]);
